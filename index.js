a343629903c6025c90238948f8127be5e461f17f
Chedjou <47749430+Chedjou@users.noreply.github.com>
2019-02-19 10:48:42 +0100

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-fbdgngngdngfng
+const express= require('express')
+
+const app=express()
+
+
+app.get('/', function(req, res){
+  res.send("hallo! Es ist nur ein test")
+})
+
+app.listen(3003, function(){
+
+  console.log ("server aktiv")
+})
